32526c32a550fc5fa66ebfcba421509c7ffd88d2
Avrami Hammer <avramihammer@gmail.com>
2018-10-14 17:38:42 +1100

    Implemented a set of processing exceptions.

---
+++ api/modules/processor/processor_exceptions.py
@@ -0,0 +1,27 @@
+import json
+
+
+class ProcessingException(Exception):
+    _exc = "Processing Failed"
+
+    detail: str
+    exc: str
+
+    def __init__(self, detail="", exc=None):
+        self.exc = self._exc
+        self.detail = detail
+
+        if exc is not None:
+            self.exc = exc
+
+    @property
+    def json(self):
+        json.dumps({
+            "status": "failed",
+            "error": self.exc,
+            "message": self.detail
+        })
+
+
+class FieldError(ProcessingException):
+    _exc = "Field Not Found"
