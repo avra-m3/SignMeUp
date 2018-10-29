b4afeb70a782b719a943745a7747046c52843262
Avrami Hammer <avramihammer@gmail.com>
2018-10-29 22:13:24 +1100

    Updated urls to reflect new design pattern Changes to keep consistency with changes in other parts of the project

--- api/main.py
+++ api/main.py
@@ -1,32 +1,34 @@
+import json
+import os
+from threading import Thread
+
 from flask import Flask
 from flask_cors import CORS
 
-from core import loop
-from model.base import BaseModel
-from objects import index, register, get_file_status, get_registration
+from model.base import BaseModel, SQLITE
+from objects import index, register, get_registration
 from utilities.router import create_routes, Route
 
 app = Flask(__name__)
 CORS(app)
 
 # Configure App from environment
+app.config["DATABASE"] = os.getenv("DATABASE") or SQLITE
+app.config["DATABASE_CONFIG"] = json.loads(os.getenv("DATABASE_CONFIG") or '{}')
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 urls = [
     Route('/', ['GET'], index),
-    Route('/request/<string:club_name>/register', ['PUT', 'POST'], register),
-    Route('/request/<string:request_id>/', ['GET'], get_file_status),
-    Route('/registration/<string:club_name>/<string:year>/<string:user_id>/', ['GET'], get_registration),
-    Route('/registration/<string:club_name>/latest/<string:user_id>/', ['GET'], get_registration),
-    Route('/registration/', ['GET'], get_registration).has_query_params(
-        ("club_name", str, False),
-        ("year", str, True),
-        ("user_id", str, True),
-    ),
+    Route('/register/<string:club_name>/', ['PUT', 'POST'], register),
+    Route('/registration/<string:registration_id>/', ['GET'], get_registration)
 ]
 
-app.before_first_request(loop)
-app.before_first_request(BaseModel.setup)
+BaseModel.setup({
+    "DATABASE": app.config["DATABASE"],
+    "DATABASE_CONFIG": app.config["DATABASE_CONFIG"],
+    "FLASK_APP": app,
+})
+
 
 create_routes(app, urls)
 
