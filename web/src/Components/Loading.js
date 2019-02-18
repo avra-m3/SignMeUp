e7f62662c6a4d5699bfbe9f1f0f2bf61713ef8b4
Avrami Hammer <avrami.hammer@rmit.edu.au>
2019-02-18 18:06:46 +1100

    UI Fixes

--- web/src/Components/Loading.js
+++ web/src/Components/Loading.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react';
 import * as PropTypes from "prop-types";
 import {withStyles} from '@material-ui/core/styles';
 import LinearProgress from "../../node_modules/@material-ui/core/LinearProgress/LinearProgress";
+import Card from "../../node_modules/@material-ui/core/Card/Card";
+import CardActionArea from "../../node_modules/@material-ui/core/CardActionArea/CardActionArea";
+import CardContent from "../../node_modules/@material-ui/core/CardContent/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
 
 
 const styles = theme => ({
@@ -11,6 +17,11 @@ const styles = theme => ({
     },
     progress: {
         flexGrow: 1,
+    },
+     paper: {
+        ...theme.mixins.gutters(),
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2,
     },
 });
 
@@ -24,12 +35,27 @@ class CardCapture extends Component {
     };
 
     render() {
-        const {classes, registration} = this.props;
+        const {classes} = this.props;
 
         return (
             <div className={classes.root}>
                 <div className={classes.progress}>
                     <LinearProgress/>
+                </div>
+                <div className={classes.paper}>
+                    <Card>
+                        <CardActionArea
+                        >
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Loading
+                                </Typography>
+                                <Typography component="p">
+                                    This might take a bit...
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
                 </div>
             </div>
         )
